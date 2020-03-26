5c3362a230f42acff8a6f3e7f7eb1cb014e7e135
Antonio Djigo <you@example.com>
2020-03-26 13:37:23 +0000

    :recycle: Refactor of route names

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
+// Licensing
+// Nookipedia's text content is released under a CC BY-SA 3.0 license.
+// When using Nookipedia's text in your application, we request that you follow the terms of the license by citing Nookipedia as the source
+// of the information, and linking back to the article's history when possible.
+
+// In the spirit of open source and knowledge, we also request that you open source your application (on a site such as GitHub) when possible.
+
 import (
-    "fmt"
-	"net/http"
-	"github.com/julienschmidt/httprouter"
+	"fmt"
 	"log"
+	"net/http"
+
 	"./content/bugs"
 	"./content/clothes"
 	"./content/decoration"
@@ -13,31 +20,30 @@ import (
 	"./content/fruits"
 	"./content/minerals"
 	"./content/villagers"
-
+	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
 
-
 	router := httprouter.New()
 	router.GET("/", Index)
-	router.GET("/bugs", bugs.BugsPage)
-	router.GET("/bugs/:bug_type", bugs.BugsByTypePage)
-	router.GET("/clothes", clothes.ClothesPage)
-	router.GET("/decoration", decoration.DecorationPage)
-	router.GET("/decoration/:decoration_type", decoration.DecorationByTypePage)
-	router.GET("/fishes", fishes.FishPage)
-	router.GET("/fossils", fossils.FossilPage)
-	router.GET("/fishes/:water_type", fishes.FishByTypePage)
-	router.GET("/fruits", fruits.FruitsPage)
-	router.GET("/minerals", minerals.MineralsPage)
-	router.GET("/villagers", villagers.VillagersPage)
-	router.GET("/villagers/:specie", villagers.VillagersByTypePage)
+	router.GET("/bugs", bugs.MainPage)
+	router.GET("/bugs/:bug_type", bugs.ByTypePage)
+	router.GET("/clothes", clothes.MainPage)
+	router.GET("/decoration", decoration.MainPage)
+	router.GET("/decoration/:decoration_type", decoration.ByTypePage)
+	router.GET("/fishes", fishes.MainPage)
+	router.GET("/fossils", fossils.MainPage)
+	router.GET("/fishes/:water_type", fishes.ByTypePage)
+	router.GET("/fruits", fruits.MainPage)
+	router.GET("/minerals", minerals.MainPage)
+	router.GET("/villagers", villagers.MainPage)
+	router.GET("/villagers/:specie", villagers.ByTypePage)
 
-
+	fmt.Println("Running server on :8080, by Djigo")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-    fmt.Fprintf(w, "Hello")
+	fmt.Fprintf(w, "Hello")
 }
